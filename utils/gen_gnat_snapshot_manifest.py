8b0c12f0fe5b72f85f6a4775493396a0d3b368d8
AldanTanneo <aldantanneo@gmail.com>
2025-11-13 11:17:17 +0100

    fix manifest indentation in publishing script

--- utils/gen_gnat_snapshot_manifest.py
+++ utils/gen_gnat_snapshot_manifest.py
@@ -56,87 +56,87 @@ for package in ("gnat", "gnatprove"):
         case "gnat":
             alire_crate = "gnat_native"
             manifest = """name = "gnat_native"
-            version = "%s"
-            provides = ["gnat=%s"]
-            description = "The GNAT Ada compiler - Native"
-            maintainers = ["Fabien Chouteau <chouteau@adacore.com>", "César Sagaert <sagaert@adacore.com>"]
-            maintainers-logins = ["Fabien-Chouteau", "AldanTanneo"]
-            licenses = "GPL-3.0-or-later AND GPL-3.0-or-later WITH GCC-exception-3.1"
-            website = "https://github.com/alire-project/GNAT-FSF-builds"
+version = "%s"
+provides = ["gnat=%s"]
+description = "The GNAT Ada compiler - Native"
+maintainers = ["Fabien Chouteau <chouteau@adacore.com>", "César Sagaert <sagaert@adacore.com>"]
+maintainers-logins = ["Fabien-Chouteau", "AldanTanneo"]
+licenses = "GPL-3.0-or-later AND GPL-3.0-or-later WITH GCC-exception-3.1"
+website = "https://github.com/alire-project/GNAT-FSF-builds"
 
-            auto-gpr-with = false
+auto-gpr-with = false
 
-            [configuration]
-            disabled = true
+[configuration]
+disabled = true
 
-            [environment."case(os)".linux]
-            PATH.prepend = "${CRATE_ROOT}/bin"
-            LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib64"
-            LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib64"
-            LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib64"
+[environment."case(os)".linux]
+PATH.prepend = "${CRATE_ROOT}/bin"
+LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib64"
+LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib64"
+LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib64"
 
-            [environment."case(os)".windows."case(host-arch)".x86-64]
-            PATH.prepend = "${CRATE_ROOT}/bin"
-            LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
-            LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
-            LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib"
+[environment."case(os)".windows."case(host-arch)".x86-64]
+PATH.prepend = "${CRATE_ROOT}/bin"
+LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
+LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
+LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib"
 
-            [environment."case(os)".macos]
-            PATH.prepend = "${CRATE_ROOT}/bin"
-            LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
-            LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
-            LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib"
-            """ % (version, version)
+[environment."case(os)".macos]
+PATH.prepend = "${CRATE_ROOT}/bin"
+LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
+LD_LIBRARY_PATH.prepend = "${CRATE_ROOT}/lib"
+LD_RUN_PATH.prepend = "${CRATE_ROOT}/lib"
+""" % (version, version)
         case "gnatprove":
             alire_crate = "gnatprove"
             manifest = """name = "gnatprove"
-            version = "%s"
-            authors = ["AdaCore"]
-            description = "Automatic formal verification of SPARK code"
-            maintainers = ["Fabien Chouteau <chouteau@adacore.com>", "César Sagaert <sagaert@adacore.com>"]
-            maintainers-logins = ["Fabien-Chouteau", "AldanTanneo"]
-            licenses = "GPL-3.0-or-later"
-            website = "https://docs.adacore.com/spark2014-docs/html/ug/index.html"
+version = "%s"
+authors = ["AdaCore"]
+description = "Automatic formal verification of SPARK code"
+maintainers = ["Fabien Chouteau <chouteau@adacore.com>", "César Sagaert <sagaert@adacore.com>"]
+maintainers-logins = ["Fabien-Chouteau", "AldanTanneo"]
+licenses = "GPL-3.0-or-later"
+website = "https://docs.adacore.com/spark2014-docs/html/ug/index.html"
 
-            long-description = \"""
-            GNATprove, which provides automatic formal verification of SPARK code, is based on the [open-source](https://github.com/AdaCore/spark2014) [SPARK Pro](https://www.adacore.com/sparkpro) by [AdaCore](https://www.adacore.com).
-            The [SPARK Pro User's Guide](https://docs.adacore.com/spark2014-docs/html/ug/index.html) provides extensive documentation on how to use GNATprove.
-            Note that because this version of GNATprove is built from an intermediate commit of SPARK Pro, it is not representative of any specific SPARK Pro release, and the SPARK Pro documentation may describe features or capabilities that are not yet available in this version of GNATprove.
+long-description = \"""
+GNATprove, which provides automatic formal verification of SPARK code, is based on the [open-source](https://github.com/AdaCore/spark2014) [SPARK Pro](https://www.adacore.com/sparkpro) by [AdaCore](https://www.adacore.com).
+The [SPARK Pro User's Guide](https://docs.adacore.com/spark2014-docs/html/ug/index.html) provides extensive documentation on how to use GNATprove.
+Note that because this version of GNATprove is built from an intermediate commit of SPARK Pro, it is not representative of any specific SPARK Pro release, and the SPARK Pro documentation may describe features or capabilities that are not yet available in this version of GNATprove.
 
-            To use GNATprove, simply add it to your Alire project using
+To use GNATprove, simply add it to your Alire project using
 
-                alr with gnatprove
+    alr with gnatprove
 
-            Then, configure your environment by running:
+Then, configure your environment by running:
 
-                eval "$( alr printenv )"
+    eval "$( alr printenv )"
 
-            You will then be able to run GNATprove:
+You will then be able to run GNATprove:
 
-                gnatprove
+    gnatprove
 
-            For more details on getting started using GNATprove, see [Getting Started with SPARK](https://docs.adacore.com/spark2014-docs/html/ug/en/getting_started.html) from the [SPARK Pro User's Guide](https://docs.adacore.com/spark2014-docs/html/ug/index.html).
+For more details on getting started using GNATprove, see [Getting Started with SPARK](https://docs.adacore.com/spark2014-docs/html/ug/en/getting_started.html) from the [SPARK Pro User's Guide](https://docs.adacore.com/spark2014-docs/html/ug/index.html).
 
-            To get started with the SPARK language, see the [Introduction to SPARK](https://learn.adacore.com/courses/intro-to-spark/index.html) course on [learn.adacore.com](https://learn.adacore.com/index.html).
-            \"""
+To get started with the SPARK language, see the [Introduction to SPARK](https://learn.adacore.com/courses/intro-to-spark/index.html) course on [learn.adacore.com](https://learn.adacore.com/index.html).
+\"""
 
-            auto-gpr-with = false
+auto-gpr-with = false
 
-            [configuration]
-            disabled = true
+[configuration]
+disabled = true
 
-            [environment]
-            PATH.prepend = "${CRATE_ROOT}/bin"
-            GPR_PROJECT_PATH.prepend = "${CRATE_ROOT}/lib/gnat"
-            """ % version
+[environment]
+PATH.prepend = "${CRATE_ROOT}/bin"
+GPR_PROJECT_PATH.prepend = "${CRATE_ROOT}/lib/gnat"
+""" % version
 
     os_template = """
 
-    [origin."case(os)".{OS}."case(host-arch)".{ARCH}]
-    binary = true
-    url = "{URL}"
-    hashes = ["{HASH}"]
-    """
+[origin."case(os)".{OS}."case(host-arch)".{ARCH}]
+binary = true
+url = "{URL}"
+hashes = ["{HASH}"]
+"""
 
     for os_name, arch, asset_identifier in [
         ("linux", "x86-64", "x86_64-linux"),
